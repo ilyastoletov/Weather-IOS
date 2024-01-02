2fd539c540d3241d879be88e094f089c9810cbad
Ilya <ilastoletov7@gmail.com>
2024-01-02 12:11:39 +0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ilya Stoletov on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
